eaee2693165331dc2f6a0512ec4a0701870f7f32
Veysel Erdemci <v.erdemci@gmail.com>
2022-01-09 10:47:35 +0300

    init project

---
+++ HB_Case.Test/UnitTest1.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace HB_Case.Test
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
---
+++ HB_CaseConsole/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HB_CaseConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
