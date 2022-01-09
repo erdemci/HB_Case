0c4947c5d4d9a067db4692db435f75065b58b975
Veysel Erdemci <v.erdemci@gmail.com>
2022-01-09 17:30:20 +0300

    Add Test cases

---
+++ HB_Case.Test/RoverTests.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+using FluentAssertions;
+using HB_Case.Execution;
+using Xunit;
+
+namespace HB_Case.Test
+{
+    public class RoverTests
+    {
+        [Theory]
+        [InlineData("MM")]
+        [InlineData("MMLR")]
+        [InlineData("LLLLMM")]
+        public void Move2NorthOn4X4MoveTwoBlocksUp(string command)
+        {
+            // Arrange
+            var plataue = new Plataeu();
+            plataue.Define(5, 5);
+            IRoverSquadManager manager = new RoverSquadManager(plataue);
+            manager.DeployRover(0, 0, Direction.N);
+            var movements = command
+                .ToCharArray()
+                .Select(x => Enum.Parse<MovementEnum>(x.ToString()))
+                .ToList();
+
+
+            // Act
+            movements.ForEach(manager.ActiveRover.Move);
+
+            // Assert
+            manager.ActiveRover.Should().NotBeNull();
+            manager.ActiveRover.X.Should().Be(0);
+            manager.ActiveRover.Y.Should().Be(2);
+            manager.ActiveRover.Direction.Should().Be(Direction.N);
+        }
+    }
+}
--- HB_CaseConsole/Program.cs
+++ HB_CaseConsole/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using HB_Case.Execution;
+using Microsoft.Extensions.DependencyInjection;
+using System;
 
 namespace HB_CaseConsole
 {
@@ -6,7 +8,19 @@ namespace HB_CaseConsole
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var serviceProvider = new ServiceCollection()
+                .AddSingleton<ILandingSurface, Plataeu>()
+                .AddSingleton<IRoverSquadManager, RoverSquadManager>()
+                .BuildServiceProvider();
+
+            var commandCenter = new CommandCenter(serviceProvider);
+            commandCenter.SendCommand("5 5");
+            commandCenter.SendCommand("1 2 N");
+            commandCenter.SendCommand("LMLMLMLMM");
+            commandCenter.SendCommand("3 3 E");
+            commandCenter.SendCommand("MMRMMRMRRM");
+
+            Console.ReadKey();
         }
     }
 }
